20db053e3169d3ede094b5fe795e081c4fbd478f
Aaron Mills <aaron@jailbreakcreative.com>
2011-04-01 23:58:55 -0400

    generated rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvn::Application.config.secret_token = '38cac2fa26ccc2596c973c4099855f68fecab29d8464e19ed7c97029ef197d30d3faa5d07cefc3bd2ad63cd9b7f0df4192ee4a4d4a15bc13a669e7a592a46d0d'
