544b2c786ff77cee3e788196ae76b52ceffbe102
Aaron Mills <aaron@jailbreakcreative.com>
2011-04-02 00:31:51 -0400

    changed to version 3.0.5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cvn::Application.config.secret_token = '38cac2fa26ccc2596c973c4099855f68fecab29d8464e19ed7c97029ef197d30d3faa5d07cefc3bd2ad63cd9b7f0df4192ee4a4d4a15bc13a669e7a592a46d0d'
+Cvn::Application.config.secret_token = '5bed4360eed0004c6a4d1d47e8de41480df7f5f6d33fd3f2015dadffd70688ab2780b5565ffa83fef8115574b701acc590368e26765d92e9f7dc9ebb8daeb316'
